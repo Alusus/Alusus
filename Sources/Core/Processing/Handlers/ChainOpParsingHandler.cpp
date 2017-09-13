29fedf201ad96b6316f973e418915a04ad677690
Sarmad Khalid Abdullah <sarmad@alusus.org>
2017-09-13 08:57:09 -0700

    Fixed source location in chain operations.

---
+++ Sources/Core/Processing/Handlers/ChainOpParsingHandler.cpp
@@ -0,0 +1,67 @@
+/**
+ * @file Core/Processing/Handlers/ChainOpParsingHandler.cpp
+ * Contains the implementation of Core::Processing::Handlers::ChainOpParsingHandler.
+ *
+ * @copyright Copyright (C) 2017 Sarmad Khalid Abdullah
+ *
+ * @license This file is released under Alusus Public License, Version 1.0.
+ * For details on usage and copying conditions read the full license in the
+ * accompanying license file or at <http://alusus.net/alusus_license_1_0>.
+ */
+//==============================================================================
+
+#include "core.h"
+
+namespace Core { namespace Processing { namespace Handlers
+{
+
+//==============================================================================
+// Overloaded Abstract Functions
+
+void ChainOpParsingHandler::addData(SharedPtr<TiObject> const &data, ParserState *state, Int levelIndex)
+{
+  if (state->isAProdRoot(levelIndex) && this->isListTerm(state, levelIndex)) {
+    if (state->refTermLevel(levelIndex).getPosId() > 1) {
+      auto myData = this->prepareToModifyData(data, state, levelIndex);
+      auto container = myData.ti_cast_get<Data::Container>();
+      if (container == 0) {
+        throw EXCEPTION(InvalidArgumentException, STR("data"),
+                        STR("Invalid object type received from chain op production"),
+                        data->getMyTypeInfo()->getUniqueName());
+      }
+      if (container->get(0) != 0) {
+        throw EXCEPTION(InvalidArgumentException, STR("data"),
+                        STR("Data is found at index 0."));
+      }
+      container->set(0, state->getData(levelIndex).get());
+
+      auto myMetadata = myData.ti_cast_get<Data::Ast::Metadata>();
+      auto srcMetadata = ti_cast<Data::Ast::Metadata>(container->get(0));
+      if (myMetadata != 0 && srcMetadata != 0) {
+        myMetadata->setSourceLocation(srcMetadata->getSourceLocation());
+      }
+
+      state->setData(myData, levelIndex);
+      return;
+    }
+  }
+  GenericParsingHandler::addData(data, state, levelIndex);
+}
+
+
+TioSharedPtr ChainOpParsingHandler::prepareToModifyData(
+  TioSharedPtr const &data, Processing::ParserState *state, Int levelIndex
+) {
+  // There is an edge case in which this is not accurate. If the state is shared at this level
+  // but not at the level where data is originated then we'll be unnecessarily cloning data.
+  // This is ok since it's an edge case and will only result in performance hit; the data will
+  // still be valid.
+  if (state->isDataShared(levelIndex)) {
+    // Duplicate the data, if possible.
+    auto clonable = data.tii_cast_get<Data::Clonable>();
+    if (clonable != 0) return clonable->clone();
+  }
+  return data;
+}
+
+} } } // namespace
--- Sources/Core/Processing/Handlers/ChainOpParsingHandler.h
+++ Sources/Core/Processing/Handlers/ChainOpParsingHandler.h
@@ -2,7 +2,7 @@
  * @file Core/Processing/Handlers/ChainOpParsingHandler.h
  * Contains the header of class Core::Processing::Handlers::ChainOpParsingHandler
  *
- * @copyright Copyright (C) 2016 Sarmad Khalid Abdullah
+ * @copyright Copyright (C) 2017 Sarmad Khalid Abdullah
  *
  * @license This file is released under Alusus Public License, Version 1.0.
  * For details on usage and copying conditions read the full license in the
@@ -37,28 +37,7 @@ class ChainOpParsingHandler : public GenericParsingHandler
   //============================================================================
   // Member Functions
 
-  protected: virtual void addData(SharedPtr<TiObject> const &data, ParserState *state, Int levelIndex)
-  {
-    if (state->isAProdRoot(levelIndex) && this->isListTerm(state, levelIndex)) {
-      if (state->refTermLevel(levelIndex).getPosId() > 1) {
-        auto myData = this->prepareToModifyData(data, state, levelIndex);
-        auto container = myData.tii_cast_get<Data::Container>();
-        if (container == 0) {
-          throw EXCEPTION(InvalidArgumentException, STR("data"),
-                          STR("Invalid object type received from chain op production"),
-                          data->getMyTypeInfo()->getUniqueName());
-        }
-        if (container->get(0) != 0) {
-          throw EXCEPTION(InvalidArgumentException, STR("data"),
-                          STR("Data is found at index 0."));
-        }
-        container->set(0, state->getData(levelIndex).get());
-        state->setData(myData, levelIndex);
-        return;
-      }
-    }
-    GenericParsingHandler::addData(data, state, levelIndex);
-  }
+  protected: virtual void addData(SharedPtr<TiObject> const &data, ParserState *state, Int levelIndex);
 
   protected: virtual Bool isListObjEnforced(ParserState *state, Int levelIndex)
   {
@@ -66,19 +45,7 @@ class ChainOpParsingHandler : public GenericParsingHandler
     else return GenericParsingHandler::isListObjEnforced(state, levelIndex);
   }
 
-  private: TioSharedPtr prepareToModifyData(TioSharedPtr const &data, Processing::ParserState *state, Int levelIndex)
-  {
-    // There is an edge case in which this is not accurate. If the state is shared at this level
-    // but not at the level where data is originated then we'll be unnecessarily cloning data.
-    // This is ok since it's an edge case and will only result in performance hit; the data will
-    // still be valid.
-    if (state->isDataShared(levelIndex)) {
-      // Duplicate the data, if possible.
-      auto clonable = data.tii_cast_get<Data::Clonable>();
-      if (clonable != 0) return clonable->clone();
-    }
-    return data;
-  }
+  private: TioSharedPtr prepareToModifyData(TioSharedPtr const &data, Processing::ParserState *state, Int levelIndex);
 
 }; // class
 
