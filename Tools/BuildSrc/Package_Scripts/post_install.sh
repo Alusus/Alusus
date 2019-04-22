b78ec53bd69a441822f8db8daf366073ea59f907
Salem Yaslem <xlmnxp@outlook.sa>
2019-04-22 03:20:23 +0300

    move apm to bin and make it executable

--- Tools/BuildSrc/Package_Scripts/post_install.sh
+++ Tools/BuildSrc/Package_Scripts/post_install.sh
@@ -14,7 +14,7 @@ fi
 
 ln -sf $(readlink -f ${ALUSUS_ROOT}/Bin/alusus) ${LINK_DIR}/alusus
 ln -sf $(readlink -f ${ALUSUS_ROOT}/Bin/الأسس) ${LINK_DIR}/الأسس
-ln -sf $(readlink -f ${ALUSUS_ROOT}/Lib/APM/apm) ${LINK_DIR}/apm
+ln -sf $(readlink -f ${ALUSUS_ROOT}/Bin/apm) ${LINK_DIR}/apm
 
 
 # Copy GTK syntax highlighting specs to gtksourceview folder.
