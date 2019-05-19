6d33d164c8ec0e82ddc46970166f6b135e9feadf
Faisal Al-Humaimidi <falhumai96@gmail.com>
2019-05-19 14:45:33 -0700

    Add option to select different Python 3 interpreter through "ALUSUS_PYTHON_INTERP" environment variable.

--- Tools/build.cmd
+++ Tools/build.cmd
@@ -1,9 +1,12 @@
 @echo off
 REM We have migrated the build scripts to Python 3.4+ (we will not support any Python version lower than that).
 setlocal
-set TEMP_PYTHON_INTERP=python
-where python3 >nul 2>&1
-if %ERRORLEVEL% EQU 0 set TEMP_PYTHON_INTERP=python3
-set SCRIPT_DIR=%~dp0
-%TEMP_PYTHON_INTERP% "%SCRIPT_DIR%\BuildSrc\build.py" %*
+REM Give users an option to select a different Python 3 interpreter.
+if %ALUSUS_PYTHON_INTERP%=="" ( 
+    set ALUSUS_PYTHON_INTERP=python
+    where python3 >nul 2>&1
+    if %ERRORLEVEL% EQU 0 set ALUSUS_PYTHON_INTERP=python3
+)
+set ALUSUS_BUILD_SCRIPT_DIR=%~dp0
+%ALUSUS_PYTHON_INTERP% "%ALUSUS_BUILD_SCRIPT_DIR%\BuildSrc\build.py" %*
 exit /b %ERRORLEVEL%
