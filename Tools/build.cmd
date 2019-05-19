9527085c829c7400609cbc5a3974871f17c692ef
Faisal Al-Humaimidi <falhumai96@gmail.com>
2019-05-19 13:47:39 -0700

    Add support for "python3.exe" executable detection.

--- Tools/build.cmd
+++ Tools/build.cmd
@@ -1,5 +1,9 @@
 @echo off
 REM We have migrated the build scripts to Python 3.4+ (we will not support any Python version lower than that).
+setlocal
+set TEMP_PYTHON_INTERP=python
+where python3 >nul 2>&1
+if %ERRORLEVEL% EQU 0 set TEMP_PYTHON_INTERP=python3
 set SCRIPT_DIR=%~dp0
-python "%SCRIPT_DIR%\BuildSrc\build.py" %*
-exit /b %errorlevel%
+%TEMP_PYTHON_INTERP% "%SCRIPT_DIR%\BuildSrc\build.py" %*
+exit /b %ERRORLEVEL%
