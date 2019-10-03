426b984a83b973f0ca1c4324352d0d2165afb40f
Faisal Al-Humaimidi <falhumai96@gmail.com>
2019-10-02 23:12:15 -0700

    Add cmd scripts for building for Windows using MSYS2/MinGW.

---
+++ Tools/mingw-w64-i686-cmd.bat
@@ -0,0 +1,8 @@
+@echo off
+setlocal
+set Path=C:\msys64\mingw32\bin;C:\msys64\mingw32\lib;C:\msys64\mingw32\i686-w64-mingw32\bin;C:\msys64\mingw32\i686-w64-mingw32\lib;C:\msys64\usr\bin;C:\msys64\usr\lib;%Path%
+set MINGW_HOST=i686-w64-mingw32
+@echo on
+cmd
+@echo off
+endlocal
---
+++ Tools/msys2-cmd.bat
@@ -0,0 +1,7 @@
+@echo off
+setlocal
+set Path=C:\msys64\usr\bin;C:\msys64\usr\lib;%Path%
+@echo on
+cmd
+@echo off
+endlocal
