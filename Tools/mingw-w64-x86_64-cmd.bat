b595f28c112abe92602004b5bd4cfc342afe1b59
Faisal Al-Humaimidi <falhumai96@gmail.com>
2020-04-08 10:23:51 -0700

    Update MSYS2/MINGW shell scripts.

--- Tools/mingw-w64-x86_64-cmd.bat
+++ Tools/mingw-w64-x86_64-cmd.bat
@@ -1,8 +1,31 @@
 @echo off
-setlocal
-set Path=C:\msys64\mingw64\bin;C:\msys64\mingw64\lib;C:\msys64\mingw64\x86_64-w64-mingw32\bin;C:\msys64\mingw64\x86_64-w64-mingw32\lib;C:\msys64\usr\bin;C:\msys64\usr\lib;C:\Program Files (x86)\Inno Setup 6;%Path%
+setlocal enabledelayedexpansion
+
+if "%MSYS_DIR%"=="" (
+    set MSYS_DIR=c:\msys64
+) else (
+    set MSYS_DIR=%MSYS_DIR%
+)
+
+if "%PATH:)=^)%"=="" (
+    set Path=!MSYS_DIR!\mingw64\bin;!MSYS_DIR!\mingw64\x86_64-w64-mingw32\bin;!MSYS_DIR!\usr\bin;!MSYS_DIR!\mingw64\lib;!MSYS_DIR!\mingw64\x86_64-w64-mingw32\lib;!MSYS_DIR!\usr\lib
+) else (
+    set Path=!MSYS_DIR!\mingw64\bin;!MSYS_DIR!\mingw64\x86_64-w64-mingw32\bin;!MSYS_DIR!\usr\bin;!MSYS_DIR!\mingw64\lib;!MSYS_DIR!\mingw64\x86_64-w64-mingw32\lib;!MSYS_DIR!\usr\lib;%PATH:)=^)%
+)
+if "%LIBRARY_PATH%"=="" (
+    set LIBRARY_PATH=!MSYS_DIR!\mingw64\lib;!MSYS_DIR!\mingw64\x86_64-w64-mingw32\lib;!MSYS_DIR!\usr\lib;
+) else (
+    set LIBRARY_PATH=!MSYS_DIR!\mingw64\lib;!MSYS_DIR!\mingw64\x86_64-w64-mingw32\lib;!MSYS_DIR!\usr\lib;%LIBRARY_PATH%
+)
+
 set MINGW_HOST=x86_64-w64-mingw32
-@echo on
-C:\Windows\System32\cmd.exe %*
-@echo off
+
+if "%MINGW_SHELL%" == "" (
+    set MINGW_SHELL=C:\Windows\System32\cmd.exe
+) else (
+    set MINGW_SHELL=%MINGW_SHELL%
+)
+
+!MINGW_SHELL! %*
+
 endlocal
--- Tools/msys2-cmd.bat
+++ Tools/msys2-cmd.bat
@@ -1,7 +1,29 @@
 @echo off
-setlocal
-set Path=C:\msys64\usr\bin;C:\msys64\usr\lib;%Path%
-@echo on
-C:\Windows\System32\cmd.exe %*
-@echo off
+setlocal enabledelayedexpansion
+
+if "%MSYS_DIR%"=="" (
+    set MSYS_DIR=c:\msys64
+) else (
+    set MSYS_DIR=%MSYS_DIR%
+)
+
+if "%PATH:)=^)%"=="" (
+    set Path=!MSYS_DIR!\usr\bin;!MSYS_DIR!\usr\lib
+) else (
+    set Path=!MSYS_DIR!\usr\bin;!MSYS_DIR!\usr\lib;%PATH:)=^)%
+)
+if "%LIBRARY_PATH%"=="" (
+    set LIBRARY_PATH=!MSYS_DIR!\usr\lib;
+) else (
+    set LIBRARY_PATH=!MSYS_DIR!\usr\lib;%LIBRARY_PATH%
+)
+
+if "%MSYS_SHELL%" == "" (
+    set MSYS_SHELL=C:\Windows\System32\cmd.exe
+) else (
+    set MSYS_SHELL=%MSYS_SHELL%
+)
+
+!MSYS_SHELL! %*
+
 endlocal
