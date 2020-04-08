35fa50f0b1822280172072bd976678c061a6d46e
Faisal Al-Humaimidi <falhumai96@gmail.com>
2020-04-08 10:58:53 -0700

    Add Inno Setup install location to the MSYS2/MINGW shell scripts.

--- Tools/msys2-cmd.bat
+++ Tools/msys2-cmd.bat
@@ -7,11 +7,18 @@ if "%MSYS_DIR%"=="" (
     set MSYS_DIR=%MSYS_DIR%
 )
 
-if "%PATH:)=^)%"=="" (
-    set Path=!MSYS_DIR!\usr\bin;!MSYS_DIR!\usr\lib
+if "%INNO_SETUP_DIR%"=="" (
+    set INNO_SETUP_DIR=C:\Program Files (x86)\Inno Setup 6
 ) else (
-    set Path=!MSYS_DIR!\usr\bin;!MSYS_DIR!\usr\lib;%PATH:)=^)%
+    set INNO_SETUP_DIR=%INNO_SETUP_DIR%
 )
+
+if "%PATH:)=^)%"=="" (
+    set Path=!MSYS_DIR!\usr\bin;!MSYS_DIR!\usr\lib;!INNO_SETUP_DIR!
+) else (
+    set Path=!MSYS_DIR!\usr\bin;!MSYS_DIR!\usr\lib;!INNO_SETUP_DIR!;%PATH:)=^)%
+)
+
 if "%LIBRARY_PATH%"=="" (
     set LIBRARY_PATH=!MSYS_DIR!\usr\lib;
 ) else (
