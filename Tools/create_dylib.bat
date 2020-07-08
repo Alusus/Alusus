1d71f426bca658bac4963372578ba6b2e22af39d
Faisal Al-Humaimidi <falhumai96@gmail.com>
2020-07-08 11:23:56 -0700

    Add scripts to create DLLs, DYLIBs, and SOs.

---
+++ Tools/create_dylib.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM We have migrated the build scripts to Python 3.4+ (we will not support any Python version lower than that).
+
+REM Set the script directory.
+for %%i in ("%~dp0.") do set "ALUSUS_BUILD_SCRIPT_DIR=%%~fi"
+
+REM Give users an option to select a different Python 3 interpreter.
+if "%ALUSUS_PYTHON_INTERP%"=="" ( 
+    set ALUSUS_PYTHON_INTERP=python
+    where python3 >nul 2>&1
+    if !ERRORLEVEL! EQU 0 (
+        set ALUSUS_PYTHON_INTERP=python3
+    )
+) else (
+    set ALUSUS_PYTHON_INTERP=%ALUSUS_PYTHON_INTERP%
+)
+
+!ALUSUS_PYTHON_INTERP! "!ALUSUS_BUILD_SCRIPT_DIR!\BuildSrc\create_dylib.py" %*
+exit /b !ERRORLEVEL!
