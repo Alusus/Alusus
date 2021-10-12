e53084ff15b6e668cf78f7a7fbc5516b2da57333
Sarmad Khalid Abdullah <sarmad@alusus.org>
2021-10-12 01:01:43 -0700

    Updated GTK syntax highlighting.
    Updated the lang spec and added custom light and dark styles
    designed around the lang spec.

--- Tools/BuildSrc/Package_Scripts/post_install.sh
+++ Tools/BuildSrc/Package_Scripts/post_install.sh
@@ -45,6 +45,24 @@ if [ "$IS_GTKSOURCEVIEW_EXISTS" -eq 1 ]; then
   cp -f ${ALUSUS_ROOT}/Tools/Gtk_Syntax_Highlighting/alusus.lang ${LANG_SPEC_DIR}/
 fi
 
+# Copy GTK styles to gtksourceview 3 folder.
+
+IS_GTKSOURCEVIEW_EXISTS=1
+if [ -e "/usr/share/gnome/gtksourceview-3.0/styles" ]; then
+  LANG_SPEC_DIR="/usr/share/gnome/gtksourceview-3.0/styles"
+elif [ -e "/usr/local/share/gtksourceview-3.0/styles" ]; then
+  LANG_SPEC_DIR="/usr/local/share/gtksourceview-3.0/styles"
+elif [ -e "/usr/share/gtksourceview-3.0/styles" ]; then
+  LANG_SPEC_DIR="/usr/share/gtksourceview-3.0/styles"
+else
+  IS_GTKSOURCEVIEW_EXISTS=0
+fi
+
+if [ "$IS_GTKSOURCEVIEW_EXISTS" -eq 1 ]; then
+  cp -f ${ALUSUS_ROOT}/Tools/Gtk_Syntax_Highlighting/alusus_light_style.xml ${LANG_SPEC_DIR}/
+  cp -f ${ALUSUS_ROOT}/Tools/Gtk_Syntax_Highlighting/alusus_dark_style.xml ${LANG_SPEC_DIR}/
+fi
+
 # Copy GTK syntax highlighting specs to gtksourceview 4 folder.
 
 IS_GTKSOURCEVIEW_EXISTS=1
@@ -60,4 +78,22 @@ fi
 
 if [ "$IS_GTKSOURCEVIEW_EXISTS" -eq 1 ]; then
   cp -f ${ALUSUS_ROOT}/Tools/Gtk_Syntax_Highlighting/alusus.lang ${LANG_SPEC_DIR}/
+fi
+
+# Copy GTK styles to gtksourceview 4 folder.
+
+IS_GTKSOURCEVIEW_EXISTS=1
+if [ -e "/usr/share/gnome/gtksourceview-4/styles" ]; then
+  LANG_SPEC_DIR="/usr/share/gnome/gtksourceview-4/styles"
+elif [ -e "/usr/local/share/gtksourceview-4/styles" ]; then
+  LANG_SPEC_DIR="/usr/local/share/gtksourceview-4/styles"
+elif [ -e "/usr/share/gtksourceview-4/styles" ]; then
+  LANG_SPEC_DIR="/usr/share/gtksourceview-4/styles"
+else
+  IS_GTKSOURCEVIEW_EXISTS=0
+fi
+
+if [ "$IS_GTKSOURCEVIEW_EXISTS" -eq 1 ]; then
+  cp -f ${ALUSUS_ROOT}/Tools/Gtk_Syntax_Highlighting/alusus_light_style.xml ${LANG_SPEC_DIR}/
+  cp -f ${ALUSUS_ROOT}/Tools/Gtk_Syntax_Highlighting/alusus_dark_style.xml ${LANG_SPEC_DIR}/
 fi
