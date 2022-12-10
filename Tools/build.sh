540e18d6de467270ae8293c6472ea4030b3107d5
Faisal Al-Humaimidi <falhumai96@gmail.com>
2022-12-10 12:05:17 -0800

    Add macOS 12 X64 GitHub workflow.

--- Tools/build.sh
+++ Tools/build.sh
@@ -4,6 +4,10 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 SCRIPT_DIR="$SCRIPT_DIR"
 shopt -s expand_aliases
-python -m pip install -r "$SCRIPT_DIR/BuildSrc/requirements.txt" &&\
-python "$SCRIPT_DIR/BuildSrc/build.py" "$@"
+PYTHON_EXECUTABLE=python3
+if [ ! $(which $PYTHON_EXECUTABLE) ]; then
+    PYTHON_EXECUTABLE=python
+fi
+$PYTHON_EXECUTABLE -m pip install -r "$SCRIPT_DIR/BuildSrc/requirements.txt" &&\
+$PYTHON_EXECUTABLE "$SCRIPT_DIR/BuildSrc/build.py" "$@"
 exit $?
